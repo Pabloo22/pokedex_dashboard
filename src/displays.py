027725a14b4ddcfc2ca50243b1e5e58931cb9d88
Pabloo22 <pablete.arino@gmail.com>
2023-06-09 15:59:23 +0200

    Add requirements.txt

--- src/displays.py
+++ src/displays.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 from st_aggrid import AgGrid, GridUpdateMode, GridOptionsBuilder, JsCode, AgGridReturn
